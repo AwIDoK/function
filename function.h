6eb40b87de6f175792ce79bb7d43019e0901b534
Andrei Filipchyk <awidokmail@gmail.com>
2019-01-26 18:32:51 +0300

    fix bugs

--- function.h
+++ function.h
@@ -12,29 +12,46 @@ namespace my {
 
     template<typename Result, typename... Args>
     class function<Result (Args...)> {
+        static const int SMALL_SIZE = 1;
         struct base_holder;
-        typedef std::array<std::byte, 16> smallT;
+        typedef std::array<std::byte, SMALL_SIZE> smallT;
         typedef std::unique_ptr<base_holder> bigT;
     public:
         function() noexcept : invoker(nullptr) {}
         function(std::nullptr_t) noexcept : invoker(nullptr) {}
         function(function const& other) : invoker(nullptr) {
             if (std::holds_alternative<smallT>(other.invoker)) {
-                invoker = std::array<std::byte, 16>();
+                if (std::holds_alternative<bigT>(invoker)) {
+                    invoker = std::array<std::byte, SMALL_SIZE>();
+                }
                 reinterpret_cast<base_holder *>(std::get<smallT>(other.invoker).data())->in_place_copy(std::get<smallT>(invoker).data());
             } else {
-                invoker = std::get<bigT>(other.invoker)->copy();
+                if (std::get<bigT>(other.invoker)) {
+                    invoker = std::get<bigT>(other.invoker)->copy();
+                } else {
+                    invoker = nullptr;
+                }
             }
         }
 
         function(function && other) noexcept {
-            swap(other);
+            if (std::holds_alternative<bigT>(other.invoker)) {
+                invoker.swap(other.invoker);
+            } else {
+                empty_small();
+                if (std::holds_alternative<bigT>(invoker)) {
+                    invoker = std::array<std::byte, SMALL_SIZE>();
+                }
+                reinterpret_cast<base_holder *>(std::get<smallT>(other.invoker).data())->in_place_copy(std::get<smallT>(invoker).data());
+            }
         }
 
         template <typename Function>
         function(Function func) : invoker(nullptr) {
             if (sizeof(holder<Function>) <= sizeof(smallT)) {
-                invoker = std::array<std::byte, 16>();
+                if (std::holds_alternative<bigT>(invoker)) {
+                    invoker = std::array<std::byte, SMALL_SIZE>();
+                }
                 new(std::get<smallT>(invoker).data()) holder<Function> (std::move(func));
             } else {
                 invoker = std::make_unique<holder<Function>>(std::move(func));
@@ -42,23 +59,40 @@ namespace my {
         }
 
         ~function() {
-            if (std::holds_alternative<smallT>(invoker)) {
-                reinterpret_cast<base_holder *>(std::get<smallT>(invoker).data())->~base_holder();
-            }
+            empty_small();
         }
 
         function& operator=(function const& other) {
+            if (&other == this) {
+                return *this;
+            }
             function(other).swap(*this);
             return *this;
         }
 
         function& operator=(function && other) noexcept {
-            swap(other);
+            if (&other == this) {
+                return *this;
+            }
+            if (std::holds_alternative<bigT>(other.invoker)) {
+                invoker.swap(other.invoker);
+            } else {
+                empty_small();
+                if (std::holds_alternative<bigT>(invoker)) {
+                    invoker = std::array<std::byte, SMALL_SIZE>();
+                }
+                ;
+                reinterpret_cast<base_holder *>(std::get<smallT>(other.invoker).data())->in_place_move(std::get<smallT>(invoker).data());
+            }
             return *this;
         }
 
         void swap(function & other) {
-            std::swap(invoker, other.invoker);
+            if (&other != this) {
+                function tmp(std::move(other));
+                other = std::move(*this);
+                *this = std::move(tmp);
+            }
         }
 
         explicit operator bool() const noexcept {
@@ -81,18 +115,22 @@ namespace my {
             virtual Result run(Args&&... args) = 0;
             virtual std::unique_ptr<base_holder> copy() = 0;
             virtual void in_place_copy(std::byte* place) const = 0;
+            virtual void in_place_move(std::byte* place) const = 0;
             void operator=(base_holder const &) = delete;
             base_holder(base_holder const &) = delete;
         };
 
         template <typename Function>
         struct holder : base_holder {
-            holder(Function func) : base_holder(), _function(func) {}
+            holder(Function func) : base_holder(), _function(std::move(func)) {}
             std::unique_ptr<base_holder> copy() override {
                 return std::make_unique<holder<Function>>(_function);
             }
             void in_place_copy(std::byte * const place) const override {
                 new(place) holder<Function>(_function);
+            }
+            void in_place_move(std::byte * const place) const override {
+                new(place) holder<Function>(std::move(_function));
             }
 
             Result run(Args&&... args) override {
@@ -105,6 +143,11 @@ namespace my {
             Function _function;
         };
         mutable std::variant<bigT, smallT> invoker;
+        void empty_small() {
+            if (std::holds_alternative<smallT>(invoker)) {
+                reinterpret_cast<base_holder *>(std::get<smallT>(invoker).data())->~base_holder();
+            }
+        }
     };
 }
 
